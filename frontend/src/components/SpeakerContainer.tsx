335848ce970c36d4479193054a6b3e9ba82a6493
Ctato1 <dznelashvilitato@gmail.com>
2024-11-30 18:02:01 +0400

    add : speaker container

--- frontend/src/components/SpeakerContainer.tsx
+++ frontend/src/components/SpeakerContainer.tsx
@@ -1,9 +1,29 @@
+import Spline from "@splinetool/react-spline";
+import AudioRecorder from "./AudioRecorder";
+import React from "react";
 
-
-const SpeakerContainer = () => {
-  return (
-    <div className="w-10 h-10 bg-black absolute top-0 left-0">SpeakerContainer</div>
-  )
+interface SpeakerContainerProps{
+    changeTalking :()=> void;
 }
 
-export default SpeakerContainer
+const SpeakerContainer:React.FC<SpeakerContainerProps> = ({changeTalking}) => {
+  return (
+    <div>
+        <div className="w-screen h-screen absolute top-0 left-0 bg-[#5555553f] z-10" onClick={changeTalking}></div>
+      <div
+        className="w-[500px] h-[500px] bg-white fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  z-20
+    rounded-xl border-[1px] border-solid flex flex-col 
+    "
+      >
+        <div className="w-[60%] h-[60%] flex m-auto rounded-[50%]">
+          <Spline scene="https://prod.spline.design/IBgfdspYaqFU2Wk3/scene.splinecode" />
+        </div>
+        <div className="w-[100%] h-[30%] ">
+          <AudioRecorder />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SpeakerContainer;
