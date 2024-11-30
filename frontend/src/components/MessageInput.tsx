0ba546d5249444d10561ea961ceae2a7b41f9a39
Ctato1 <dznelashvilitato@gmail.com>
2024-11-30 16:03:22 +0400

    fix audio error

--- frontend/src/components/MessageInput.tsx
+++ frontend/src/components/MessageInput.tsx
@@ -31,26 +31,6 @@ const MessageInput: React.FC<MessageInputProps> = ({
 
   return (
     <div className="w-full max-w-4xl mx-auto pb-3">
-      {messages.length == 0 && (
-        <div
-          className="grid gap-3 p-4 grid-cols-1  sm:grid-cols-2 "
-        >
-          <button className="p-2 sm:p-4  text-black rounded-xl border-[1px] border-solid transition-all duration-300   hover:scale-105 text-[0.6rem] sm:text-[1rem] lg:text-[1.2rem]" data-value="How many planets are there in our solar system?"
-            onClick={(e) => setInput(e.currentTarget.getAttribute("data-value") || "")}>How many planets are there in our solar system?
-          </button>
-          <button className="p-2 sm:p-4   text-black rounded-xl border-[1px] border-solid transition-all duration-300   hover:scale-105 text-[0.6rem] sm:text-[1.1rem] lg:text-[1.2rem]" 
-            data-value="What is the square root of 144?" onClick={(e) => setInput(e.currentTarget.getAttribute("data-value") || "")}
-          >What is the square root of 144?</button>
-          <button className="p-2 sm:p-4   text-black rounded-xl border-[1px] border-solid transition-all duration-300   hover:scale-105 text-[0.6rem] sm:text-[1.1rem] lg:text-[1.2rem]
-           hidden sm:block "
-           data-value="Who is the current President of the United States?" onClick={(e) => setInput(e.currentTarget.getAttribute("data-value") || "")}
-           >Who is the current President of the United States?</button>
-          <button className="p-2 sm:p-4   text-black rounded-xl border-[1px] border-solid transition-all duration-300   hover:scale-105 text-[0.6rem] sm:text-[1.1rem] lg:text-[1.2rem] 
-          hidden sm:block"
-          data-value="Who wrote 'To Kill a Mockingbird'?" onClick={(e) => setInput(e.currentTarget.getAttribute("data-value") || "")}
-          >Who wrote "To Kill a Mockingbird"?</button>
-        </div>
-      )}
       <div className="flex items-center justify-between bg-white  rounded-2xl p-1 space-x-4 w-[80%] sm:w-[100%] sm:p-3 m-auto shadow-2xl shadow-gray-500/50 bg-gray-200">
         <textarea
           className={`outline-none rounded-lg p-3 text-lg transition-all w-[80%] resize-none bg-gray-200 color-white${
